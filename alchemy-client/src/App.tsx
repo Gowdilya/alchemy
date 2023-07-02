11d3423e67439a9f776dfd08351698e0c23849a8
Gowdilya Jeyakumar <gowdilyajeyakumar@Gowdilyas-MacBook-Pro-2.local>
2023-07-01 20:58:22 -0400

    Added custom loader component and error handling

--- alchemy-client/src/App.tsx
+++ alchemy-client/src/App.tsx
@@ -2,33 +2,62 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import RGBAlchemy from "./components/RGBAlchemy";
+import Loading from "./components/Loading";
 
 function App() {
   const [data, setData] = useState();
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  async function loadGame() {
-    const response = await fetch("http://localhost:9876/init");
-    const jsonData = await response.json();
-    setData(jsonData);
+  async function loadGameData() {
+    try {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:9876/init");
+      if (!response.ok) {
+        throw new Error("Failed to fetch game data");
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
   }
 
-  async function loadGameUser(userId: string) {
-    const response = await fetch("http://localhost:9876/init/user/" + userId);
-    const jsonData = await response.json();
-    let newData = { ...jsonData };
-    setData(newData);
+  async function loadUserGameData(userId: string) {
+    try {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:9876/init/user/" + userId);
+      if (!response.ok) {
+        throw new Error("Failed to fetch user game data");
+      }
+      const jsonData = await response.json();
+      let newData = { ...jsonData };
+      setData(newData);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
   }
   const playGameAgain = (userId: string) => {
-    loadGameUser(userId);
+    loadUserGameData(userId);
   };
 
   useEffect(() => {
-    loadGame();
+    loadGameData();
   }, []);
 
   return (
     <div className="App">
-      {data ? <RGBAlchemy data={data} handlePlayAgain={playGameAgain} /> : null}
+      {isLoading ? (
+        <Loading />
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        data && <RGBAlchemy data={data} handlePlayAgain={playGameAgain} />
+      )}
     </div>
   );
 }
---
+++ alchemy-client/src/components/Loading/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import Square from "../BasicShapes/Square";
+
+function Loading() {
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+  const colors = [
+    [0, 0, 0],
+    [255, 0, 0],
+    [0, 255, 0],
+    [0, 0, 255],
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className="loading">
+      <Square color={colors[currentColorIndex]} />
+      <Square color={colors[(currentColorIndex + 1) % colors.length]} />
+      <Square color={colors[(currentColorIndex + 2) % colors.length]} />
+    </div>
+  );
+}
+
+export default Loading;
